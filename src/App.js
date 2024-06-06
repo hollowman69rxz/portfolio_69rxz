87e2e9def0b8938acba3a6ac66e7f413954e65fd
hollowman69rxz <stuard6@yahoo.co.id>
2024-06-07 00:28:20 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+// import Login from './components/Login';
+// import './App.css';
+
+function App() {
+    return (
+        <Router>
+            <div className="App">
+                <nav>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/portfolio">Portfolio</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                        {/* <li className="login-link"><Link to="/login">Login</Link></li> */}
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/portfolio" element={<Portfolio />} />
+                    <Route path="/contact" element={<Contact />} />
+                    {/* <Route path="/login" element={<Login />} /> */}
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+export default App;

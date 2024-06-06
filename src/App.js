2bef6f98375f0bb7f2351b1ca2d9d76416b4009d
hollowman69rxz <stuard6@yahoo.co.id>
2024-06-07 01:40:07 +0700

    create folder components and the page contents

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
-import Portfolio from './components/Portfolio';
+import Project from './components/Project';
 import Contact from './components/Contact';
 // import Login from './components/Login';
-// import './App.css';
+import './App.css';
 
 function App() {
     return (
@@ -15,7 +15,7 @@ function App() {
                     <ul>
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/about">About</Link></li>
-                        <li><Link to="/portfolio">Portfolio</Link></li>
+                        <li><Link to="/project">Project</Link></li>
                         <li><Link to="/contact">Contact</Link></li>
                         {/* <li className="login-link"><Link to="/login">Login</Link></li> */}
                     </ul>
@@ -23,7 +23,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
-                    <Route path="/portfolio" element={<Portfolio />} />
+                    <Route path="/project" element={<Project />} />
                     <Route path="/contact" element={<Contact />} />
                     {/* <Route path="/login" element={<Login />} /> */}
                 </Routes>

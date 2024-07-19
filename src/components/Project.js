444c6dbbf3b15caa80e0ee5b416ab5b2d2e60561
hollowman69rxz <stuard6@yahoo.co.id>
2024-07-20 01:29:58 +0700

    update for appearance for Project.js and add some Styles.css: add separator, add button view project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Styles.css'; // Import custom CSS for Portfolio component styling
 import HomeImage from './img/Home.png'; // Import the image
+import NewProjectImage from './img/UMI_Project.png'; // Import the new project image
 
 const Project = () => {
   // Dummy data for portfolio items
@@ -9,20 +10,31 @@ const Project = () => {
       title: "React js : portfolio_69rxz",
       description: "This project aims to demonstrate my skills to potential employers during the hiring process.",
       skills: "HTML, CSS, JavaScript",
-      image: HomeImage // Use the imported image
+      image: HomeImage, // Use the imported image
+      link: "http://localhost:3000/" // Link to the project
+    },
+    {
+      title: "Codeigniter 3: SMD",
+      description: "Proyek ini adalah proyek yang kemungkinan akan dikerjakan untuk PT UNI METALTECH INDONESIA",
+      skills: "Codeigniter 3, Node.js",
+      image: NewProjectImage, // Use the new imported image
+      link: "http://localhost/documis/" // Link to the new project
     }
   ];
 
   return (
-    <div className="project-container">
-      <h1 className="section-heading">Projects</h1>
-      <div className="section-content">
+    <div className="kanban-container">
+      <h1 className="section-heading">All Running Projects</h1>
+      <div className="kanban-board">
         {projectItems.map((item, index) => (
-          <div key={index}>
+          <div key={index} className="kanban-card">
             <h2>{item.title}</h2>
             <img src={item.image} alt={item.title} className="thumbnail" /> {/* Display the image */}
             <p>Description: {item.description}</p>
             <p>Skills: {item.skills}</p>
+            <a href={item.link} target="_blank" rel="noopener noreferrer">
+              <button className="project-button">View Project</button>
+            </a>
           </div>
         ))}
       </div>
